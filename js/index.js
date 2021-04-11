0bb521d3c47918daae7c2231f937f61668d931ce
Felix Isaac <limfelix16@gmail.com>
2021-04-12 03:21:15 +0800

    feat: add main buttons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
+let displayElement = document.getElementById("display");
+let calculator = {
+	screen: '',
+	get display () {
+		return this.screen;
+	}
+}
 
+document.querySelectorAll("div.buttons button")
+	.forEach((button) => {
+		button.addEventListener("click", ({ target }) => {
+			input(target.innerHTML);
+		})
+	})
+
+const input = (value) => {
+	switch(value) {
+		case "DEL": {
+			calculator.display = screen.substring(0, screen.length - 1);
+			break;
+		}
+		case "AC": {
+			calculator.display = "";
+			break;
+		}
+		default: {
+			calculator.display += value;
+			break;
+		}
+	}
+}
